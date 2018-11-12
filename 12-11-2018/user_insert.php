62fa4434937065f0bc9e374eaf27fd15f3dc9c7a
gaurav-ewb <44963010+gaurav-ewb@users.noreply.github.com>
2018-11-12 16:47:19 +0530

    Create user_insert.php

---
+++ 12-11-2018/user_insert.php
@@ -0,0 +1,26 @@
+<?php 
+include('connection.php');
+
+$fname=$_REQUEST['fname'];
+$lname=$_REQUEST['lname'];
+$email=$_REQUEST['email'];
+$password=md5($_REQUEST['password']);
+$phone=$_REQUEST['phone'];
+$address=$_REQUEST['address'];
+
+$query = "SELECT * FROM user WHERE email = '$email'";
+$result = mysqli_query($con,$query);
+if ($result) {
+  if (mysqli_num_rows($result) > 0) {
+  	 echo 'found!';
+  }
+  else {
+echo $query=mysqli_query($con,"INSERT INTO `user` (`fname`, `lname`, `email`, `password`, `phone`, `address`) VALUES ('$fname', '$lname', '$email',  '$password', '$phone', '$address')"); 
+ }
+} else {
+  echo 'Error: '.mysqli_error();
+}
+?>
+
+
+
